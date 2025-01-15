e6c20f51a92c026733e42b617dc2d2f52e5e4ec9
Hossein <hossein.taji@epfl.ch>
2025-01-15 13:43:22 +0100

    normalize kernel

--- sw/applications/transformer_kernels/normalize/addNormC.h
+++ sw/applications/transformer_kernels/normalize/addNormC.h
@@ -9,7 +9,7 @@
 #include <stdint.h>
 // #include <stdlib.h>
 #include "math.h"
-#include "../param.h"
+#include "param.h"
 
 typedef struct {
     int seq_len_;
--- sw/applications/transformer_kernels/normalize/main.c
+++ sw/applications/transformer_kernels/normalize/main.c
@@ -2,41 +2,37 @@
 #include "timer_sdk.h"
 #include "core_v_mini_mcu.h"
 #include "addNormC.h"
+#include "defines.h"
 
 #define PRINT
 
-#define SEQ_LEN 128
+#define SEQ_LEN 32//128
 #define INPUT_DIM 64
 
 #define INPUT_SIZE SEQ_LEN * INPUT_DIM
 #define WEIGHT_SIZE INPUT_DIM
 #define BIAS_SIZE INPUT_DIM
 
-
+// allocate in this section: (section(".xheep_data_interleaved")
+quant_bit_width __attribute__((section(".xheep_data_interleaved"))) input[INPUT_SIZE] = {0};
+quant_bit_width __attribute__((section(".xheep_data_interleaved"))) weight[WEIGHT_SIZE] = {0};
+quant_bit_width __attribute__((section(".xheep_data_interleaved"))) bias[BIAS_SIZE] = {0};
+quant_bit_width __attribute__((section(".xheep_data_interleaved"))) input_normalized[INPUT_SIZE] = {0};
 
 
 int main() {
 
-    // allocate in this section: (section(".xheep_data_interleaved")
-    quant_bit_width __attribute__((section(".xheep_data_interleaved"))) input[INPUT_SIZE] = {0};
-    quant_bit_width __attribute__((section(".xheep_data_interleaved"))) weight[WEIGHT_SIZE] = {0};
-    quant_bit_width __attribute__((section(".xheep_data_interleaved"))) bias[BIAS_SIZE] = {0};
-    quant_bit_width __attribute__((section(".xheep_data_interleaved"))) input_normalized[INPUT_SIZE] = {0};
-
-    int seq_len = SEQ_LEN; // Example sequence length
-    int input_dim = INPUT_DIM; // Example input dimension
-
     // Initialize input, weight, and bias with some values
-    for (int i = 0; i < seq_len * input_dim; i++) {
-        input[i] = i % 256; // Example initialization
+    for (int i = 0; i < INPUT_SIZE; i++) {
+        input[i] = i; // Example initialization
     }
-    for (int i = 0; i < input_dim; i++) {
+    for (int i = 0; i < INPUT_DIM; i++) {
         weight[i] = 1; // Example initialization
         bias[i] = 0; // Example initialization
     }
 
     // Create AddNormalize structure
-    AddNormalize addNorm = createAddNormalize(seq_len, input_dim, weight, bias);
+    AddNormalize addNorm = createAddNormalize(SEQ_LEN, INPUT_DIM, weight, bias);
 
 
     #ifdef PRINT
@@ -54,3 +50,15 @@ int main() {
 
     return 0;
 }
+
+
+    // quant_bit_width *ptr_input = input;
+
+    // for (quant_bit_width i = 0; i < 20; i++) {
+    //     input[i] = i;
+    // }
+    // // Print input for the int16_t format
+    // for (int i = 0; i < 5; i++) {
+    //     printf("%d\n", *ptr_input);
+    //     ptr_input = ptr_input + 1;        
+    // }
